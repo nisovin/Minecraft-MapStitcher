4cc5ddf7fe8af6e06556e2f72594084bfda07eb4
Nisovin <nisovin@gmail.com>
2018-07-08 17:30:06 -0600

    First commit

---
+++ config.py
@@ -0,0 +1,14 @@
+### CONFIG FILE ###
+
+# The folder with the map_#.dat files in it.
+inputDir = "/home/user/server/world/data/"
+
+# The filename of the generated image.
+outputFile = "/home/user/map.png"
+
+# The bounding coordinates of the generated map.
+xmin = -3000
+zmin = -3000
+xmax = 3000
+zmax = 3000
+
